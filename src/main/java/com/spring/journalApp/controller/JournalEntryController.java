2367c4b15905d4a51a153ae18ea167032592d025
Your Name <you@example.com>
2024-08-02 15:23:02 +0530

    RequestMapping concept

--- src/main/java/com/spring/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/spring/journalApp/controller/JournalEntryController.java
@@ -1,8 +1,24 @@
 package com.spring.journalApp.controller;
 
+import com.spring.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/route")
 public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntryMap = new HashMap<>();
+
+    @GetMapping("/journal")
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntryMap.values());
+    }
 
 }
