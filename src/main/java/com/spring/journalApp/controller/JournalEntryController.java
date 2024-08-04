29c080b3b5b50fa2485b5a479d12113b7746523d
Your Name <you@example.com>
2024-08-04 23:59:43 +0530

    postmapping

--- src/main/java/com/spring/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/spring/journalApp/controller/JournalEntryController.java
@@ -1,9 +1,7 @@
 package com.spring.journalApp.controller;
 
 import com.spring.journalApp.entity.JournalEntry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,9 +14,14 @@ public class JournalEntryController {
 
     private Map<Long, JournalEntry> journalEntryMap = new HashMap<>();
 
-    @GetMapping("/journal")
+    @GetMapping
     public List<JournalEntry> getAll() {
         return new ArrayList<>(journalEntryMap.values());
     }
 
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntryMap.put(myEntry.getId(), myEntry);
+        return true;
+    }
 }
--- src/main/java/com/spring/journalApp/entity/JournalEntry.java
+++ src/main/java/com/spring/journalApp/entity/JournalEntry.java
@@ -6,7 +6,7 @@ public class JournalEntry {
     private String title;
     private String content;
 
-    public Integer getId() {
+    public long getId() {
         return id;
     }
 
